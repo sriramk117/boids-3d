6a4a67e80eab68b311288b7010d9f3895f039e07
SoundCloudRapper365 <sriramku@outlook.com>
2024-06-14 15:34:27 -0700

    fixed limiter and vector addition errors

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ camera.position.z = 5;
 controls.update();
 
 const boids = [];
-var num_boids = 50;
-var separation = 0.3;
+var num_boids = 100;
+var separation = 0.5;
 var lim_v = new THREE.Vector3(0.07, 0.07, 0.07);
 var visual_range = 0.8;
 var x_bound = 5;
@@ -53,9 +53,9 @@ function animate() {
 function initialize_positions() {
     // Initialize the positions of the boids
     for (var i = 0; i < num_boids; i++) {
-        boids[i].position.x = (Math.random() * 2 - 1) * 5;
-        boids[i].position.y = (Math.random() * 2 - 1) * 5;
-        boids[i].position.z = (Math.random() * 2 - 1) * 5;
+        boids[i].position.x = (Math.random() * 2 - 1) * 10;
+        boids[i].position.y = (Math.random() * 2 - 1) * 10;
+        boids[i].position.z = (Math.random() * 2 - 1) * 10;
     }
 }
 
@@ -84,38 +84,44 @@ function rule1(j)  {
 
 function rule2(j) {
     // Rule 2: Boids try to keep a small distance away from other objects (including other boids)
+    var factor = 0.05;
     var displacement = new THREE.Vector3();
     var dist = new THREE.Vector3();
 
     for (let i = 0; i < num_boids; i++) {
         if (j != i && boids[i].position.distanceTo(boids[j].position) < separation) {
-            dist.subVectors(boids[i].position, boids[j].position);
-            displacement.sub(dist);
+            dist.subVectors(boids[j].position, boids[i].position);
+            displacement.add(dist);
         }
     }
 
-    return displacement;
+    return displacement.multiplyScalar(factor);
 }
 
 function rule3(j) {
     // Rule 3: Boids try to match velocity with near boids
+    var factor = 0.125;
     var avg_velocity = new THREE.Vector3();
+    var neighbours = 0;
 
     for (let i = 0; i < num_boids; i++) {
         if (j != i && boids[i].position.distanceTo(boids[j].position) < visual_range) {
             avg_velocity.add(boids[i].velocity);
+            neighbours++;
         }
     }
-
-    avg_velocity.divideScalar(num_boids - 1);
+    if (neighbours == 0) {
+        return new THREE.Vector3();
+    }
+    avg_velocity.divideScalar(neighbours);
     avg_velocity.sub(boids[j].velocity);
-    return avg_velocity.divideScalar(8);
+    return avg_velocity.multiplyScalar(factor);
 
 }
 
 function bound(j) {
     var limiter = new THREE.Vector3();
-    var limiter_constant = 0.05; 
+    var limiter_constant = 0.02; 
     if (boids[j].position.x > x_bound) {
         limiter.x = -limiter_constant;
     } else if (boids[j].position.x < -x_bound) {
@@ -127,7 +133,7 @@ function bound(j) {
         limiter.y = limiter_constant;
     }
     if (boids[j].position.z > z_bound) {
-        limiter.z = limiter_constant;
+        limiter.z = -limiter_constant;
     } else if (boids[j].position.z < -z_bound) {
         limiter.z = limiter_constant;
     }
@@ -151,15 +157,11 @@ function update_boids() {
     var v4;
 
     for (let i = 0; i < num_boids; i++) {
-        v1 = rule1(i);
-        v2 = rule2(i);
-        v3 = rule3(i);
-        v4 = bound(i);
+        v1 = rule1(i); boids[i].velocity.add(v1);
+        v2 = rule2(i); boids[i].velocity.add(v2);
+        v3 = rule3(i); boids[i].velocity.add(v3);
+        v4 = bound(i); boids[i].velocity.add(v4);
 
-        boids[i].velocity.add(v1);
-        boids[i].velocity.add(v2);
-        boids[i].velocity.add(v3);
-        boids[i].velocity.add(v4);
         limit_velocity(i);
 
         boids[i].position.add(boids[i].velocity);
